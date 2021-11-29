51119d3e231d06ed9a317d3962acad20f6b16b04
Antoine <contact@brokestudio.fr>
2021-11-29 20:12:01 +0100

    server update

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const PORT = 1235;
 
 const server = dgram.createSocket("udp4");
 const client = dgram.createSocket("udp4");
-let intervals = [];
+let interval, id, value;
 
 server.on("listening", function() {
   let address = server.address();
@@ -29,33 +29,29 @@ server.on("listening", function() {
 });
 
 server.on("message", (data, remote) => {
-  let id = `${remote.address}:${remote.port}`;
-  //console.log(data);
-  if(intervals[id] === undefined)
+  let newID = `${remote.address}:${remote.port}`;
+  if(id !== newID)
   {
-    console.log(`connection from ${id}`);
-    intervals[id] = {};
-    intervals[id].value = 0;
-    intervals[id].interval = setInterval(() => {
-        let arr = [];
-        for (let index = 1; index < 151; index++) {
-          arr.push(index);
-        }
-        arr.push(intervals[id].value);
-        //console.log('send');
-        const data = new Uint8Array(arr);
-        client.send(data, 0, data.length, remote.port, remote.address);
-        intervals[id].value++;
-        //intervals[id].value &= 0xff;
-        if(intervals[id].value > 255) {
-          intervals[id].value = 0;
-        }
-        //clearInterval( intervals[id].interval ); //intervals[id].value = 0;
-      }, 50);
+    if(id !== undefined) console.log(`unregistering ${id}`);
+    id = newID;
+    console.log(`registering ${id}`);
+    value = 0;
+    if(interval !== undefined) clearInterval(interval);
+    interval = setInterval(() => {
+      let arr = [];
+      for (let index = 1; index < 151; index++) {
+        arr.push(index);
+      }
+      arr.push(value);
+      const data = new Uint8Array(arr);
+      client.send(data, 0, data.length, remote.port, remote.address);
+      value++;
+      value &= 0xff;
+    }, 1);
   }
   else
   {
-    console.log(`already connected : ${id}`)
+    console.log(`message from ${id}`);
   }
 });
 
